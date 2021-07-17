bd3faec15d55f029c4c6fef87a758208ebd6563d
glorious73 <amjadmh73@gmail.com>
2021-07-17 23:50:29 +0300

    Implemented methods and adjusted CourseViewModel

---
+++ CleanArch.Infra.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDbContext _context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses; // Ideally: try, catch, log, and throw.
+        }
+    }
+}

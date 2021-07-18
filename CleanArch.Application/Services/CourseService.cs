b9c0051e23780c4b43b37159930aad8a7ebdfb52
glorious73 <amjadmh73@gmail.com>
2021-07-18 22:11:27 +0300

    Create course functionality using MediatR. Updated CourseViewModel.

--- CleanArch.Application/Services/CourseService.cs
+++ CleanArch.Application/Services/CourseService.cs
@@ -1,5 +1,7 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -9,11 +11,23 @@ namespace CleanArch.Application.Services
 {
     public class CourseService : ICourseService
     {
-        private ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
+        private readonly ICourseRepository _courseRepository;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(IMediatorHandler bus, ICourseRepository courseRepository)
         {
+            _bus = bus;
             _courseRepository = courseRepository;
+        }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                courseViewModel.Name,
+                courseViewModel.Description,
+                courseViewModel.ImageUrl
+                );
+            _bus.SendCommand(createCourseCommand);
         }
 
         public CourseViewModel GetCourses()

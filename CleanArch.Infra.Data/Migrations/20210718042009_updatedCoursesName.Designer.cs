ae89f111cc3efb8b1029290d49af647c814def79
glorious73 <amjadmh73@gmail.com>
2021-07-18 07:24:21 +0300

    Updated courses table name in db

--- CleanArch.Infra.Data/Migrations/20210718042009_updatedCoursesName.Designer.cs
+++ CleanArch.Infra.Data/Migrations/20210718042009_updatedCoursesName.Designer.cs
@@ -3,14 +3,16 @@ using CleanArch.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CleanArch.Infra.Data.Migrations
 {
     [DbContext(typeof(UniversityDbContext))]
-    partial class UniversityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210718042009_updatedCoursesName")]
+    partial class updatedCoursesName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
